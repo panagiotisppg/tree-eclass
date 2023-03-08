4626ca5535454fe578e029b169cebe476c6f4f79
Basil Papadimas <basil@excession.gr>
2023-03-08 18:55:01 +0200

    all the old changes squashed

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # tree-eclass
+## _Eclass File Downloading and File Finder_
+
+tree-eclass is a python project used for auto-updating and downloading the latest 
+files and folders for a subject from eclass
+
+## Features
+
+- Auto-update university folder (upcoming versions)
+- Command line tree of subject's files and folders
+
+## Installation
+
+tree-eclass requires [Python3](https://python.org/) to run.
+
+Install the required libraries.
+
+```sh
+git clone https://github.com/panagiotisppg/tree-eclass.git
+cd tree-eclass
+pip3 install -r requirements.txt
+```
+
+##### Warning
+This code in it's current version is only used for public [eclass](https://aueb.eclass.gr) classes
---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+from src.tree_utils import *
+
+if __name__ == "__main__":
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description='Generate a eclass.aueb directory tree for a given the INF number')
+    parser.add_argument('-t', '--tab-string', type=str, default='\t', help='The string used for indentation (default: \\t)')
+    parser.add_argument('-I', '--INF', type=str, required=True, help='The eclass INF number to generate the directory tree from')
+    args = parser.parse_args()
+    url = f"https://eclass.aueb.gr/modules/document/?course=INF{args.INF}"
+    # Generate the directory tree
+    tree = gen_subtree(url)
+    print('Generating tree structure')
+    # Print the directory tree
+    print_tree(tree, tab_string=args.tab_string)
